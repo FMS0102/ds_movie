ce464ab37248e3350d19f6704ffe0b70fb246f98
Felipe M. Simões <44732534+FMS0102@users.noreply.github.com>
2022-01-13 17:48:41 -0300

    Project created

---
+++ backend/src/main/java/com/fms/sds_movie/SdsMovieApplication.java
@@ -0,0 +1,13 @@
+package com.fms.sds_movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdsMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdsMovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/fms/sds_movie/SdsMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fms.sds_movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdsMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
