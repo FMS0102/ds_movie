d9d6360b5a15d702f6272af9b2f7a3c97c1fe12a
Felipe M. Simões <44732534+FMS0102@users.noreply.github.com>
2022-01-14 16:55:51 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/fms/sds_movie/entities/Movie.java
@@ -0,0 +1,32 @@
+package com.fms.sds_movie.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Data
+@Entity
+@Table(name = "tb_movie")
+public class Movie implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private Double score;
+    private Integer count;
+    private String Image;
+}
---
+++ backend/src/main/java/com/fms/sds_movie/entities/ScorePK.java
@@ -0,0 +1,25 @@
+package com.fms.sds_movie.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+@Embeddable
+public class ScorePK implements Serializable{
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
